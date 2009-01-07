4072acc458e12892c7b0bb890de5eec741994510
brianf <brianf@13f79535-47bb-0310-9956-ffa450edef68>
2009-01-07 15:17:43 +0000

    [maven-release-plugin]  copy for tag maven-dependency-analyzer-1.1

---
+++ src/main/java/org/apache/maven/shared/dependency/analyzer/DefaultProjectDependencyAnalyzer.java
@@ -0,0 +1,231 @@
+package org.apache.maven.shared.dependency.analyzer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * 
+ * 
+ * @author <a href="mailto:markhobson@gmail.com">Mark Hobson</a>
+ * @version $Id$
+ * @plexus.component role="org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer"
+ */
+public class DefaultProjectDependencyAnalyzer
+    implements ProjectDependencyAnalyzer
+{
+    // fields -----------------------------------------------------------------
+
+    /**
+     * ClassAnalyzer
+     * 
+     * @plexus.requirement
+     */
+    private ClassAnalyzer classAnalyzer;
+
+    /**
+     * DependencyAnalyzer
+     * 
+     * @plexus.requirement
+     */
+    private DependencyAnalyzer dependencyAnalyzer;
+
+    // ProjectDependencyAnalyzer methods --------------------------------------
+
+    /*
+     * @see org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer#analyze(org.apache.maven.project.MavenProject)
+     */
+    public ProjectDependencyAnalysis analyze( MavenProject project )
+        throws ProjectDependencyAnalyzerException
+    {
+        try
+        {
+            Map artifactClassMap = buildArtifactClassMap( project );
+
+            Set dependencyClasses = buildDependencyClasses( project );
+
+            Set declaredArtifacts = buildDeclaredArtifacts( project );
+            
+            Set usedArtifacts = buildUsedArtifacts( artifactClassMap, dependencyClasses );
+            
+            Set usedDeclaredArtifacts = new LinkedHashSet( declaredArtifacts );
+            usedDeclaredArtifacts.retainAll( usedArtifacts );
+
+            Set usedUndeclaredArtifacts = new LinkedHashSet( usedArtifacts );
+            usedUndeclaredArtifacts = removeAll( usedUndeclaredArtifacts, declaredArtifacts );
+
+            Set unusedDeclaredArtifacts = new LinkedHashSet( declaredArtifacts );
+            unusedDeclaredArtifacts = removeAll( unusedDeclaredArtifacts, usedArtifacts );
+
+            return new ProjectDependencyAnalysis( usedDeclaredArtifacts, usedUndeclaredArtifacts,
+                                                  unusedDeclaredArtifacts );
+        }
+        catch ( IOException exception )
+        {
+            throw new ProjectDependencyAnalyzerException( "Cannot analyze dependencies", exception );
+        }
+    }
+
+    /**
+     * This method defines a new way to remove the artifacts by using the
+     * conflict id. We don't care about the version here because there can be
+     * only 1 for a given artifact anyway.
+     * 
+     * @param start
+     *            initial set
+     * @param remove
+     *            set to exclude
+     * @return set with remove excluded
+     */
+    private Set removeAll( Set start, Set remove )
+    {
+        Set results = new LinkedHashSet( start.size() );
+        Iterator iter = start.iterator();
+        while ( iter.hasNext() )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            Iterator iter2 = remove.iterator();
+            boolean found = false;
+            while ( iter2.hasNext() )
+            {
+                Artifact artifact2 = (Artifact) iter2.next();
+                if ( artifact.getDependencyConflictId().equals( artifact2.getDependencyConflictId() ) )
+                {
+                    found = true;
+                }
+            }
+            if ( !found )
+            {
+                results.add( artifact );
+            }
+        }
+        return results;
+    }
+
+    // private methods --------------------------------------------------------
+
+    private Map buildArtifactClassMap( MavenProject project )
+        throws IOException
+    {
+        Map artifactClassMap = new LinkedHashMap();
+
+        Set dependencyArtifacts = project.getArtifacts();
+
+        for ( Iterator iterator = dependencyArtifacts.iterator(); iterator.hasNext(); )
+        {
+            Artifact artifact = (Artifact) iterator.next();
+
+            File file = artifact.getFile();
+
+            if ( file != null && file.getName().endsWith( ".jar" ) )
+            {
+                URL url = file.toURL();
+
+                Set classes = classAnalyzer.analyze( url );
+
+                artifactClassMap.put( artifact, classes );
+            }
+        }
+
+        return artifactClassMap;
+    }
+
+    private Set buildDependencyClasses( MavenProject project )
+        throws IOException
+    {
+        Set dependencyClasses = new HashSet();
+        
+        String outputDirectory = project.getBuild().getOutputDirectory();
+        dependencyClasses.addAll( buildDependencyClasses( outputDirectory ) );
+        
+        String testOutputDirectory = project.getBuild().getTestOutputDirectory();
+        dependencyClasses.addAll( buildDependencyClasses( testOutputDirectory ) );
+
+        return dependencyClasses;
+    }
+    
+    private Set buildDependencyClasses( String path )
+        throws IOException
+    {
+        URL url = new File( path ).toURI().toURL();
+
+        return dependencyAnalyzer.analyze( url );
+    }
+    
+    private Set buildDeclaredArtifacts( MavenProject project )
+    {
+        Set declaredArtifacts = project.getDependencyArtifacts();
+        
+        if ( declaredArtifacts == null )
+        {
+            declaredArtifacts = Collections.EMPTY_SET;
+        }
+        
+        return declaredArtifacts;
+    }
+    
+    private Set buildUsedArtifacts( Map artifactClassMap, Set dependencyClasses )
+    {
+        Set usedArtifacts = new HashSet();
+
+        for ( Iterator dependencyIterator = dependencyClasses.iterator(); dependencyIterator.hasNext(); )
+        {
+            String className = (String) dependencyIterator.next();
+
+            Artifact artifact = findArtifactForClassName( artifactClassMap, className );
+
+            if ( artifact != null )
+            {
+                usedArtifacts.add( artifact );
+            }
+        }
+        
+        return usedArtifacts;
+    }
+
+    private Artifact findArtifactForClassName( Map artifactClassMap, String className )
+    {
+        for ( Iterator artifactIterator = artifactClassMap.keySet().iterator(); artifactIterator.hasNext(); )
+        {
+            Artifact artifact = (Artifact) artifactIterator.next();
+
+            Set artifactClassNames = (Set) artifactClassMap.get( artifact );
+
+            if ( artifactClassNames.contains( className ) )
+            {
+                return artifact;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/org/apache/maven/shared/dependency/analyzer/ProjectDependencyAnalysis.java
@@ -0,0 +1,157 @@
+package org.apache.maven.shared.dependency.analyzer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * 
+ * 
+ * @author <a href="mailto:markhobson@gmail.com">Mark Hobson</a>
+ * @version $Id$
+ */
+public class ProjectDependencyAnalysis
+{
+    // fields -----------------------------------------------------------------
+
+    private final Set usedDeclaredArtifacts;
+
+    private final Set usedUndeclaredArtifacts;
+
+    private final Set unusedDeclaredArtifacts;
+
+    // constructors -----------------------------------------------------------
+    
+    public ProjectDependencyAnalysis()
+    {
+        this( null, null, null );
+    }
+
+    public ProjectDependencyAnalysis( Set usedDeclaredArtifacts, Set usedUndeclaredArtifacts,
+                                      Set unusedDeclaredArtifacts )
+    {
+        this.usedDeclaredArtifacts = safeCopy(usedDeclaredArtifacts);
+        this.usedUndeclaredArtifacts = safeCopy(usedUndeclaredArtifacts);
+        this.unusedDeclaredArtifacts = safeCopy(unusedDeclaredArtifacts);
+    }
+
+    // public methods ---------------------------------------------------------
+
+    public Set getUsedDeclaredArtifacts()
+    {
+        return usedDeclaredArtifacts;
+    }
+
+    public Set getUsedUndeclaredArtifacts()
+    {
+        return usedUndeclaredArtifacts;
+    }
+
+    public Set getUnusedDeclaredArtifacts()
+    {
+        return unusedDeclaredArtifacts;
+    }
+    
+    // Object methods ---------------------------------------------------------
+    
+    /*
+     * @see java.lang.Object#hashCode()
+     */
+    public int hashCode()
+    {
+        int hashCode = getUsedDeclaredArtifacts().hashCode();
+        hashCode = (hashCode * 37) + getUsedUndeclaredArtifacts().hashCode();
+        hashCode = (hashCode * 37) + getUnusedDeclaredArtifacts().hashCode();
+        
+        return hashCode;
+    }
+    
+    /*
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals( Object object )
+    {
+        boolean equals;
+        
+        if ( object instanceof ProjectDependencyAnalysis )
+        {
+            ProjectDependencyAnalysis analysis = (ProjectDependencyAnalysis) object;
+            
+            equals = getUsedDeclaredArtifacts().equals( analysis.getUsedDeclaredArtifacts() )
+                && getUsedUndeclaredArtifacts().equals( analysis.getUsedUndeclaredArtifacts() )
+                && getUnusedDeclaredArtifacts().equals( analysis.getUnusedDeclaredArtifacts() );
+        }
+        else
+        {
+            equals = false;
+        }
+        
+        return equals;
+    }
+    
+    /*
+     * @see java.lang.Object#toString()
+     */
+    public String toString()
+    {
+        StringBuffer buffer = new StringBuffer();
+        
+        if ( !getUsedDeclaredArtifacts().isEmpty() )
+        {
+            buffer.append( "usedDeclaredArtifacts=" ).append( getUsedDeclaredArtifacts() );
+        }
+        
+        if ( !getUsedUndeclaredArtifacts().isEmpty() )
+        {
+            if ( buffer.length() > 0)
+            {
+                buffer.append( "," );
+            }
+            
+            buffer.append( "usedUndeclaredArtifacts=" ).append( getUsedUndeclaredArtifacts() );
+        }
+        
+        if ( !getUnusedDeclaredArtifacts().isEmpty() )
+        {
+            if ( buffer.length() > 0)
+            {
+                buffer.append( "," );
+            }
+            
+            buffer.append( "unusedDeclaredArtifacts=" ).append( getUnusedDeclaredArtifacts() );
+        }
+
+        buffer.insert( 0, "[" );
+        buffer.insert( 0, getClass().getName() );
+        
+        buffer.append( "]" );
+        
+        return buffer.toString();
+    }
+    
+    // private methods --------------------------------------------------------
+    
+    private Set safeCopy( Set set )
+    {
+        return ( set == null ) ? Collections.EMPTY_SET : Collections.unmodifiableSet( new LinkedHashSet( set ) );
+    }
+}
